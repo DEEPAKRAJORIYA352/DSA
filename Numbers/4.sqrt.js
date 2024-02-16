d9528ed58013fa1875c495cf4100b2e771a738dd
DEEPAK RAJORIYA <136314499+DEEPAKRAJORIYA352@users.noreply.github.com>
2024-02-16 18:05:56 +0530

    Add files via upload

---
+++ Numbers/4.sqrt.js
@@ -0,0 +1,15 @@
+
+let num = 10;
+
+function squreRoot(num){
+    let ans;
+    for(let i=1; i<=num; i++){
+
+        if((i*i)<=num) {
+            
+            ans = i;
+        }  
+   }
+   console.log(ans);
+}
+squreRoot(num);
---
+++ Numbers/5.divisor.js
@@ -0,0 +1,11 @@
+
+let num;
+
+function Divisor(num){
+    for(let i=1; i<=num; i++){
+        if((num%i)==0){
+            console.log(i);
+        }
+     }
+}
+Divisor(25);
